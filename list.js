55ccecfdb024fa1972061131bc169ada545260d7
pecherin.m <pecherin.m@promail366.com>
2024-02-09 09:26:24 +0200

    Adds new items for parsing

--- list.js
+++ list.js
@@ -1,56 +1,104 @@
 export default [
-    // {
-    //     name: 'Vue github',
-    //     url: 'https://github.com/vuejs/core/releases',
-    //     selector: '.clearfix .Box .Box-body .Link--primary',
-    //     content: 'v3.4.16'
-    // },
-    // {
-    //     name: 'React github',
-    //     url: 'https://github.com/facebook/react/releases',
-    //     selector: '.clearfix .Box .Box-body .Link--primary',
-    //     content: '18.2.0 (June 14, 2022)'
-    // },
-    // {
-    //     name: 'Angular github',
-    //     url: 'https://github.com/angular/angular/releases',
-    //     selector: '.clearfix .Box .Box-body .Link--primary',
-    //     content: 'v17.2.0-rc.0'
-    // },
-    // {
-    //     name: 'Svelte github',
-    //     url: 'https://github.com/sveltejs/svelte/releases',
-    //     selector: '.clearfix .Box .Box-body .Link--primary',
-    //     content: 'svelte@5.0.0-next.51'
-    // },
-    // {
-    //     name: 'Solid github',
-    //     url: 'https://github.com/solidjs/solid/releases',
-    //     selector: '.clearfix .Box .Box-body .Link--primary',
-    //     content: 'v1.8.0 - Bifröst'
-    // },
-    // {
-    //     name: 'iQuery github',
-    //     url: 'https://github.com/jquery/jquery/releases',
-    //     selector: '.clearfix .Box .Box-body .Link--primary',
-    //     content: 'jQuery 4.0.0 BETA!'
-    // },
-    // {
-    //     name: 'Backbone github',
-    //     url: 'https://github.com/jashkenas/backbone/tags',
-    //     selector: '.clearfix .Box .Box-body .Link--primary',
-    //     content: '1.6.0'
-    // },
     {
-        name: 'Node.js LTS',
+        name: 'Vue github',
+        url: 'https://github.com/vuejs/core/releases',
+        selector: '.clearfix .Box .Box-body .Link--primary',
+        content: 'v3.4.18',
+    },
+    {
+        name: 'React github',
+        url: 'https://github.com/facebook/react/releases',
+        selector: '.clearfix .Box .Box-body .Link--primary',
+        content: '18.2.0 (June 14, 2022)'
+    },
+    {
+        name: 'Angular github',
+        url: 'https://github.com/angular/angular/releases',
+        selector: '.clearfix .Box .Box-body .Link--primary',
+        content: 'v17.2.0-rc.0'
+    },
+    {
+        name: 'Svelte github',
+        url: 'https://github.com/sveltejs/svelte/releases',
+        selector: '.clearfix .Box .Box-body .Link--primary',
+        content: 'svelte@5.0.0-next.51'
+    },
+    {
+        name: 'Solid github',
+        url: 'https://github.com/solidjs/solid/releases',
+        selector: '.clearfix .Box .Box-body .Link--primary',
+        content: 'v1.8.0 - Bifröst'
+    },
+    {
+        name: 'iQuery github',
+        url: 'https://github.com/jquery/jquery/releases',
+        selector: '.clearfix .Box .Box-body .Link--primary',
+        content: 'jQuery 4.0.0 BETA!'
+    },
+    {
+        name: 'Backbone github',
+        url: 'https://github.com/jashkenas/backbone/tags',
+        selector: '.clearfix .Box .Box-body .Link--primary',
+        content: '1.6.0'
+    },
+    {
+        name: 'Node LTS',
         url: 'https://nodejs.org/en/download/',
         selector: '.color-lightgray strong',
         content: '20.11.0'
     },
     {
-        name: 'Node.js LTS',
+        name: 'Node Current',
         url: 'https://nodejs.org/en/download/current',
         selector: '.color-lightgray strong',
         content: '21.6.1'
+    },
+    {
+        name: 'NPM github',
+        url: 'https://github.com/npm/cli/releases',
+        selector: '.clearfix .Box .Box-body .Link--primary',
+        content: 'v10.4.0'
+    },
+    {
+        name: 'Ember github',
+        url: 'https://github.com/emberjs/ember.js/releases',
+        selector: '.clearfix .Box .Box-body .Link--primary',
+        content: 'v5.7.0-beta.1'
+    },
+    {
+        name: 'Meteor changelog',
+        url: 'https://docs.meteor.com/changelog',
+        selector: '.document-formatting h2',
+        content: 'v2.15.0, 2024-02-05'
+    },
+    {
+        name: 'Polymer github',
+        url: 'https://github.com/Polymer/polymer/releases',
+        selector: '.clearfix .Box .Box-body .Link--primary',
+        content: 'v3.4.1'
+    },
+    {
+        name: 'Aurelia github',
+        url: 'https://github.com/aurelia/framework/releases',
+        selector: '.clearfix .Box .Box-body .Link--primary',
+        content: '1.4.1'
+    },
+    {
+        name: 'Next github',
+        url: 'https://github.com/vercel/next.js/releases',
+        selector: '.clearfix .Box .Box-body .Link--primary',
+        content: 'v14.1.1-canary.44'
+    },
+    {
+        name: 'Express changelog',
+        url: 'https://expressjs.com/en/changelog/4x.html',
+        selector: '#page-doc h2',
+        content: '4.18.1 - Release date: 2022-04-29'
+    },
+    {
+        name: 'Express github',
+        url: 'https://github.com/expressjs/express/releases',
+        selector: '.clearfix .Box .Box-body .Link--primary',
+        content: '4.18.2'
     },
 ]
