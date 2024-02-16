202f00c7a619ecb50862ed9c63074cc5e1364212
pecherin.m <pecherin.m@promail366.com>
2024-02-16 11:10:37 +0200

    Adds Docker Desktop release notes

--- list.js
+++ list.js
@@ -1,236 +1,242 @@
 export default [
+    // {
+    //     name: 'Vue github',
+    //     url: 'https://github.com/vuejs/core/releases',
+    //     selector: '.clearfix .Box .Box-body .Link--primary',
+    //     content: 'v3.4.19',
+    // },
+    // {
+    //     name: 'React github',
+    //     url: 'https://github.com/facebook/react/releases',
+    //     selector: '.clearfix .Box .Box-body .Link--primary',
+    //     content: '18.2.0 (June 14, 2022)'
+    // },
+    // {
+    //     name: 'Angular github',
+    //     url: 'https://github.com/angular/angular/releases',
+    //     selector: '.clearfix .Box .Box-body .Link--primary',
+    //     content: 'v17.2.1'
+    // },
+    // {
+    //     name: 'Angular blog',
+    //     url: 'https://blog.angular.io/',
+    //     selector: '.row h3 div',
+    //     content: 'Angular v17.2 is now available'
+    // },
+    // {
+    //     name: 'Svelte github',
+    //     url: 'https://github.com/sveltejs/svelte/releases',
+    //     selector: '.clearfix .Box .Box-body .Link--primary',
+    //     content: 'svelte@4.2.11'
+    // },
+    // {
+    //     name: 'Solid github',
+    //     url: 'https://github.com/solidjs/solid/releases',
+    //     selector: '.clearfix .Box .Box-body .Link--primary',
+    //     content: 'v1.8.0 - Bifröst'
+    // },
+    // {
+    //     name: 'iQuery github',
+    //     url: 'https://github.com/jquery/jquery/releases',
+    //     selector: '.clearfix .Box .Box-body .Link--primary',
+    //     content: 'jQuery 4.0.0 BETA!'
+    // },
+    // {
+    //     name: 'Backbone github',
+    //     url: 'https://github.com/jashkenas/backbone/tags',
+    //     selector: '.clearfix .Box .Box-body .Link--primary',
+    //     content: '1.6.0'
+    // },
+    // {
+    //     name: 'Node LTS',
+    //     url: 'https://nodejs.org/en/download/',
+    //     selector: '.color-lightgray strong',
+    //     content: '20.11.1'
+    // },
+    // {
+    //     name: 'Node blog',
+    //     url: 'https://nodejs.org/en/blog/release',
+    //     selector: '.blog-index a',
+    //     content: 'Node v21.6.2 (Current)'
+    // },
+    // {
+    //     name: 'Node Current',
+    //     url: 'https://nodejs.org/en/download/current',
+    //     selector: '.color-lightgray strong',
+    //     content: '21.6.2'
+    // },
+    // {
+    //     name: 'NPM github',
+    //     url: 'https://github.com/npm/cli/releases',
+    //     selector: '.clearfix .Box .Box-body .Link--primary',
+    //     content: 'v10.4.0'
+    // },
+    // {
+    //     name: 'Ember github',
+    //     url: 'https://github.com/emberjs/ember.js/releases',
+    //     selector: '.clearfix .Box .Box-body .Link--primary',
+    //     content: 'v5.7.0-beta.1'
+    // },
+    // {
+    //     name: 'Meteor changelog',
+    //     url: 'https://docs.meteor.com/changelog',
+    //     selector: '.document-formatting h2',
+    //     content: 'v2.15.0, 2024-02-05'
+    // },
+    // {
+    //     name: 'Polymer github',
+    //     url: 'https://github.com/Polymer/polymer/releases',
+    //     selector: '.clearfix .Box .Box-body .Link--primary',
+    //     content: 'v3.4.1'
+    // },
+    // {
+    //     name: 'Aurelia github',
+    //     url: 'https://github.com/aurelia/framework/releases',
+    //     selector: '.clearfix .Box .Box-body .Link--primary',
+    //     content: '1.4.1'
+    // },
+    // {
+    //     name: 'Next github',
+    //     url: 'https://github.com/vercel/next.js/releases',
+    //     selector: '.clearfix .Box .Box-body .Link--primary',
+    //     content: 'v14.1.1-canary.56'
+    // },
+    // {
+    //     name: 'Express changelog',
+    //     url: 'https://expressjs.com/en/changelog/4x.html',
+    //     selector: '#page-doc h2',
+    //     content: '4.18.1 - Release date: 2022-04-29'
+    // },
+    // {
+    //     name: 'Express github',
+    //     url: 'https://github.com/expressjs/express/releases',
+    //     selector: '.clearfix .Box .Box-body .Link--primary',
+    //     content: '4.18.2'
+    // },
+    // {
+    //     name: 'TypeScript github',
+    //     url: 'https://github.com/microsoft/TypeScript/releases',
+    //     selector: '.clearfix .Box .Box-body .Link--primary',
+    //     content: 'TypeScript 5.4 Beta'
+    // },
+    // {
+    //     name: 'TypeScript news',
+    //     url: 'https://devblogs.microsoft.com/typescript/',
+    //     selector: '.post .entry-title a',
+    //     content: 'Announcing TypeScript 5.4 Beta'
+    // },
+    // {
+    //     name: 'Google chrome releases',
+    //     url: 'https://chromereleases.googleblog.com/',
+    //     selector: '.post .publishdate',
+    //     content: 'Thursday, February 15, 2024'
+    // },
+    // {
+    //     name: 'Bun.js github',
+    //     url: 'https://github.com/oven-sh/bun/releases',
+    //     selector: '.clearfix .Box .Box-body .Link--primary',
+    //     content: 'Bun v1.0.26'
+    // },
+    // {
+    //     name: 'Bun.js blog',
+    //     url: 'https://bun.sh/blog',
+    //     selector: '.BlogCardContents .text-gray-900',
+    //     content: 'Bun v1.0.26'
+    // },
+    // {
+    //     name: 'Firefox releases',
+    //     url: 'https://www.mozilla.org/en-US/firefox/releases/',
+    //     selector: '.c-release-list li',
+    //     content: '122.0 122.0.1'
+    // },
+    // {
+    //     name: 'HTMX github',
+    //     url: 'https://github.com/bigskysoftware/htmx/releases',
+    //     selector: '.clearfix .Box .Box-body .Link--primary',
+    //     content: 'v1.9.10'
+    // },
+    // {
+    //     name: 'Vite github',
+    //     url: 'https://github.com/vitejs/vite/releases',
+    //     selector: '.clearfix .Box .Box-body .Link--primary',
+    //     content: 'v5.1.3'
+    // },
+    // {
+    //     name: 'Vite blog',
+    //     url: 'https://vitejs.dev/blog',
+    //     selector: '.blog-entry .title a',
+    //     content: 'Announcing Vite 5.1'
+    // },
+    // {
+    //     name: 'Astro blog',
+    //     url: 'https://astro.build/blog/',
+    //     selector: '.code.text-astro-gray-200',
+    //     content: 'February 15, 2024'
+    // },
+    // {
+    //     name: 'Nuxt github',
+    //     url: 'https://github.com/nuxt/nuxt/releases',
+    //     selector: '.clearfix .Box .Box-body .Link--primary',
+    //     content: 'v3.10.2'
+    // },
+    // {
+    //     name: 'Nuxt blog',
+    //     url: 'https://nuxt.com/blog',
+    //     selector: '.flex .grid .text-gray-900',
+    //     content: 'Nuxt 3.10'
+    // },
+    // {
+    //     name: 'Biome github',
+    //     url: 'https://github.com/biomejs/biome/releases',
+    //     selector: '.clearfix .Box .Box-body .Link--primary',
+    //     content: 'CLI v1.5.3-nightly.dca6a7a'
+    // },
+    // {
+    //     name: 'Biome blog',
+    //     url: 'https://biomejs.dev/blog/',
+    //     selector: '.blog h2 a',
+    //     content: 'Biome v1.5'
+    // },
+    // {
+    //     name: 'Qwik github',
+    //     url: 'https://github.com/BuilderIO/qwik/releases',
+    //     selector: '.clearfix .Box .Box-body .Link--primary',
+    //     content: 'v1.4.4'
+    // },
+    // {
+    //     name: 'Qwik blog',
+    //     url: 'https://www.builder.io/blog',
+    //     selector: '.css-1kve7vp .css-rfu6vm .css-o4l6sf',
+    //     content: 'Towards Qwik 2.0: Lighter, Faster, Better'
+    // },
+    // {
+    //     name: 'Qwik blog latest',
+    //     url: 'https://www.builder.io/blog',
+    //     selector: '.css-1kve7vp .css-enxx6z',
+    //     content: 'Export Figma to Tailwind CSS with Visual Copilot'
+    // },
+    // {
+    //     name: 'ESLint blog',
+    //     url: 'https://eslint.org/blog/',
+    //     selector: '.blog-posts .card  .card__title a',
+    //     content: 'ESLint v9.0.0-beta.0 released'
+    // },
+    // {
+    //     name: 'ESLint github',
+    //     url: 'https://github.com/eslint/eslint/releases',
+    //     selector: '.clearfix .Box .Box-body .Link--primary',
+    //     content: 'v9.0.0-beta.0'
+    // },
+    // {
+    //     name: 'Panda github',
+    //     url: 'https://github.com/chakra-ui/panda/releases',
+    //     selector: '.clearfix .Box .Box-body .Link--primary',
+    //     content: '@pandacss/types@0.31.0'
+    // },
     {
-        name: 'Vue github',
-        url: 'https://github.com/vuejs/core/releases',
-        selector: '.clearfix .Box .Box-body .Link--primary',
-        content: 'v3.4.19',
-    },
-    {
-        name: 'React github',
-        url: 'https://github.com/facebook/react/releases',
-        selector: '.clearfix .Box .Box-body .Link--primary',
-        content: '18.2.0 (June 14, 2022)'
-    },
-    {
-        name: 'Angular github',
-        url: 'https://github.com/angular/angular/releases',
-        selector: '.clearfix .Box .Box-body .Link--primary',
-        content: 'v17.2.1'
-    },
-    {
-        name: 'Angular blog',
-        url: 'https://blog.angular.io/',
-        selector: '.row h3 div',
-        content: 'Angular v17.2 is now available'
-    },
-    {
-        name: 'Svelte github',
-        url: 'https://github.com/sveltejs/svelte/releases',
-        selector: '.clearfix .Box .Box-body .Link--primary',
-        content: 'svelte@4.2.11'
-    },
-    {
-        name: 'Solid github',
-        url: 'https://github.com/solidjs/solid/releases',
-        selector: '.clearfix .Box .Box-body .Link--primary',
-        content: 'v1.8.0 - Bifröst'
-    },
-    {
-        name: 'iQuery github',
-        url: 'https://github.com/jquery/jquery/releases',
-        selector: '.clearfix .Box .Box-body .Link--primary',
-        content: 'jQuery 4.0.0 BETA!'
-    },
-    {
-        name: 'Backbone github',
-        url: 'https://github.com/jashkenas/backbone/tags',
-        selector: '.clearfix .Box .Box-body .Link--primary',
-        content: '1.6.0'
-    },
-    {
-        name: 'Node LTS',
-        url: 'https://nodejs.org/en/download/',
-        selector: '.color-lightgray strong',
-        content: '20.11.1'
-    },
-    {
-        name: 'Node blog',
-        url: 'https://nodejs.org/en/blog/release',
-        selector: '.blog-index a',
-        content: 'Node v21.6.2 (Current)'
-    },
-    {
-        name: 'Node Current',
-        url: 'https://nodejs.org/en/download/current',
-        selector: '.color-lightgray strong',
-        content: '21.6.2'
-    },
-    {
-        name: 'NPM github',
-        url: 'https://github.com/npm/cli/releases',
-        selector: '.clearfix .Box .Box-body .Link--primary',
-        content: 'v10.4.0'
-    },
-    {
-        name: 'Ember github',
-        url: 'https://github.com/emberjs/ember.js/releases',
-        selector: '.clearfix .Box .Box-body .Link--primary',
-        content: 'v5.7.0-beta.1'
-    },
-    {
-        name: 'Meteor changelog',
-        url: 'https://docs.meteor.com/changelog',
-        selector: '.document-formatting h2',
-        content: 'v2.15.0, 2024-02-05'
-    },
-    {
-        name: 'Polymer github',
-        url: 'https://github.com/Polymer/polymer/releases',
-        selector: '.clearfix .Box .Box-body .Link--primary',
-        content: 'v3.4.1'
-    },
-    {
-        name: 'Aurelia github',
-        url: 'https://github.com/aurelia/framework/releases',
-        selector: '.clearfix .Box .Box-body .Link--primary',
-        content: '1.4.1'
-    },
-    {
-        name: 'Next github',
-        url: 'https://github.com/vercel/next.js/releases',
-        selector: '.clearfix .Box .Box-body .Link--primary',
-        content: 'v14.1.1-canary.56'
-    },
-    {
-        name: 'Express changelog',
-        url: 'https://expressjs.com/en/changelog/4x.html',
-        selector: '#page-doc h2',
-        content: '4.18.1 - Release date: 2022-04-29'
-    },
-    {
-        name: 'Express github',
-        url: 'https://github.com/expressjs/express/releases',
-        selector: '.clearfix .Box .Box-body .Link--primary',
-        content: '4.18.2'
-    },
-    {
-        name: 'TypeScript github',
-        url: 'https://github.com/microsoft/TypeScript/releases',
-        selector: '.clearfix .Box .Box-body .Link--primary',
-        content: 'TypeScript 5.4 Beta'
-    },
-    {
-        name: 'TypeScript news',
-        url: 'https://devblogs.microsoft.com/typescript/',
-        selector: '.post .entry-title a',
-        content: 'Announcing TypeScript 5.4 Beta'
-    },
-    {
-        name: 'Google chrome releases',
-        url: 'https://chromereleases.googleblog.com/',
-        selector: '.post .publishdate',
-        content: 'Thursday, February 15, 2024'
-    },
-    {
-        name: 'Bun.js github',
-        url: 'https://github.com/oven-sh/bun/releases',
-        selector: '.clearfix .Box .Box-body .Link--primary',
-        content: 'Bun v1.0.26'
-    },
-    {
-        name: 'Bun.js blog',
-        url: 'https://bun.sh/blog',
-        selector: '.BlogCardContents .text-gray-900',
-        content: 'Bun v1.0.26'
-    },
-    {
-        name: 'Firefox releases',
-        url: 'https://www.mozilla.org/en-US/firefox/releases/',
-        selector: '.c-release-list li',
-        content: '122.0 122.0.1'
-    },
-    {
-        name: 'HTMX github',
-        url: 'https://github.com/bigskysoftware/htmx/releases',
-        selector: '.clearfix .Box .Box-body .Link--primary',
-        content: 'v1.9.10'
-    },
-    {
-        name: 'Vite github',
-        url: 'https://github.com/vitejs/vite/releases',
-        selector: '.clearfix .Box .Box-body .Link--primary',
-        content: 'v5.1.3'
-    },
-    {
-        name: 'Vite blog',
-        url: 'https://vitejs.dev/blog',
-        selector: '.blog-entry .title a',
-        content: 'Announcing Vite 5.1'
-    },
-    {
-        name: 'Astro blog',
-        url: 'https://astro.build/blog/',
-        selector: '.code.text-astro-gray-200',
-        content: 'February 15, 2024'
-    },
-    {
-        name: 'Nuxt github',
-        url: 'https://github.com/nuxt/nuxt/releases',
-        selector: '.clearfix .Box .Box-body .Link--primary',
-        content: 'v3.10.2'
-    },
-    {
-        name: 'Nuxt blog',
-        url: 'https://nuxt.com/blog',
-        selector: '.flex .grid .text-gray-900',
-        content: 'Nuxt 3.10'
-    },
-    {
-        name: 'Biome github',
-        url: 'https://github.com/biomejs/biome/releases',
-        selector: '.clearfix .Box .Box-body .Link--primary',
-        content: 'CLI v1.5.3-nightly.dca6a7a'
-    },
-    {
-        name: 'Biome blog',
-        url: 'https://biomejs.dev/blog/',
-        selector: '.blog h2 a',
-        content: 'Biome v1.5'
-    },
-    {
-        name: 'Qwik github',
-        url: 'https://github.com/BuilderIO/qwik/releases',
-        selector: '.clearfix .Box .Box-body .Link--primary',
-        content: 'v1.4.4'
-    },
-    {
-        name: 'Qwik blog',
-        url: 'https://www.builder.io/blog',
-        selector: '.css-1kve7vp .css-rfu6vm .css-o4l6sf',
-        content: 'Towards Qwik 2.0: Lighter, Faster, Better'
-    },
-    {
-        name: 'Qwik blog latest',
-        url: 'https://www.builder.io/blog',
-        selector: '.css-1kve7vp .css-enxx6z',
-        content: 'Export Figma to Tailwind CSS with Visual Copilot'
-    },
-    {
-        name: 'ESLint blog',
-        url: 'https://eslint.org/blog/',
-        selector: '.blog-posts .card  .card__title a',
-        content: 'ESLint v9.0.0-beta.0 released'
-    },
-    {
-        name: 'ESLint github',
-        url: 'https://github.com/eslint/eslint/releases',
-        selector: '.clearfix .Box .Box-body .Link--primary',
-        content: 'v9.0.0-beta.0'
-    },
-    {
-        name: 'Panda github',
-        url: 'https://github.com/chakra-ui/panda/releases',
-        selector: '.clearfix .Box .Box-body .Link--primary',
-        content: '@pandacss/types@0.31.0'
+        name: 'Docker Desktop release notes',
+        url: 'https://docs.docker.com/desktop/release-notes/',
+        selector: '.prose h2 a',
+        content: '4.27.2'
     },
 ]
